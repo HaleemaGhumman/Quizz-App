fc3787fe68a7b35c69c5fff0b5779627c7e1c4ec
haleema <haleemaghumman943@gmail.com>
2025-07-18 11:39:27 +0500

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
-import react from '@vitejs/plugin-react'
-import svgr from 'vite-plugin-svgr'
-import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react';
+import svgr from 'vite-plugin-svgr';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  plugins: [react(), svgr()],
   build: {
     outDir: 'dist',
     rollupOptions: {
       input: {
-        main: 'index.html', // Ensure this matches the file name exactly
+        main: 'index.html',
       },
     },
   },
